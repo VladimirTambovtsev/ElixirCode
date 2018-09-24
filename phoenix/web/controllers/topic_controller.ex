1f9c91194f1eda3b2ff58c40446be85484e77646
Vladimir Tambovtsev <tambovcev99@mail.ru>
2018-09-24 16:26:29 +0300

    migrated model to psql

---
+++ phoenix/web/controllers/topic_controller.ex
@@ -0,0 +1,11 @@
+defmodule Discuss.TopicController do
+	use Discuss.Web, :controller
+
+	alias Discuss.Topic
+
+	def new(conn, params) do
+		struct = %Topic{}
+		params = %{}
+		changeset = Topic.changeset(%Topic{}, %{})
+	end
+end
