796b60a5e26d4d935e79fa933d228def56794aef
Vladimir Tambovtsev <tambovcev99@mail.ru>
2018-09-25 14:52:33 +0300

    OAuth implemented, ui bug fixed

--- phoenix/config/config.exs
+++ phoenix/config/config.exs
@@ -25,3 +25,13 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+	providers: [
+		github: { Ueberauth.Strategy.Github, [] }
+	]
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+	client_id: "cec2e1c19135b509eb4c",
+	client_secret: "e46050a382d7bb1b4ab1dbc80227f776719fc9f0"
+
+
