92320c7bd4cb5144022df90b04b75b4ab94d6f94
Vladimir Tambovtsev <tambovcev99@mail.ru>
2018-09-29 23:12:36 +0300

    serve routes

--- pure_ex/lib/servy/handler.ex
+++ pure_ex/lib/servy/handler.ex
@@ -2,12 +2,15 @@ defmodule Servy.Handler do
 	def handle(request) do
 		request 
 		|> parse 
+		|> log
 		|> route 
 		|> format_response
 	end
 
+	def log(conv), do: IO.inspect conv
+
 	def parse(request) do
-		[method, path, protocol] = 
+		[method, path, _] =		# _ = request.protocol
 			request 
 			|> String.split("\n") 
 			|> List.first
@@ -17,22 +20,31 @@ defmodule Servy.Handler do
 	end
 
 	def route(conv) do
-		conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+		route(conv, conv.method, conv.path)
 	end
+
+	def route(conv, "GET", "/wildthings") do
+		%{ conv | resp_body: "Bears, Lions, Tigers" }
+	end
+
+	def route(conv, "GET", "/bears") do
+		%{ conv | resp_body: "Bears list" }
+	end
+
 
 	def format_response(conv) do
 		"""
 		HTTP/1.1 200 OK
 		Content-Type: text/html
-		Content-Length: 20
+		Content-Length: #{String.length(conv.resp_body)}
 
-		Bears, Lions, Tigers
+		#{conv.resp_body}
 		"""
 	end
 end
 
 request = """
-GET /wildthings HTTP/1.1
+GET /bears HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
