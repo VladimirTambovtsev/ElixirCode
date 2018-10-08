7bc4f16e8842b0ff3080feb766f44b545f161d3a
Vladimir Tambovtsev <tambovcev99@mail.ru>
2018-10-08 16:52:11 +0300

    check jwt auth headers

--- gql/graphql_api/lib/graphql_api/guardian.ex
+++ gql/graphql_api/lib/graphql_api/guardian.ex
@@ -3,18 +3,22 @@ defmodule GraphqlApi.Guardian do
 
 	alias GraphqlApi.Accounts
 
-	def subject_for_token(user, _claims) do
-		sub = to_string(user.id)
-		{:ok, sub}
+	def subject_for_token(%Accounts.User{} = user, _claims) do
+		{:ok, to_string(user.id)}
 	end
 
 	def subject_for_token(_, _) do
 		{:error, :reason_for_error}
 	end
 
-	def resource_from_claims(claims) do
-		user = claims["sub"]
-			|> Accounts.get_user!()
-		{:ok, user}
+	def resource_from_claims(%{"sub" => id}) do
+		case Accounts.get_user!(id) do
+			nil -> {:error, :resource_not_found}
+			user -> {:ok, user}
+		end
+	end
+
+	def resource_from_claims(_claims) do
+		{:error, :reason_for_error}
 	end
 end
